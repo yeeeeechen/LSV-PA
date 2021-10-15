09019f7fafa205dbcbeea1d13d0744e2a36f8fa5
Whyu1125 <kobe7682477@gmail.com>
2021-10-16 01:53:22 +0800

    Finish_PA1

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,11 +1,14 @@
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
-
+#include <set>
+using namespace std;
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
+static int Lsv_MSFC(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_msfc", Lsv_MSFC, 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -15,6 +18,117 @@ Abc_FrameInitializer_t frame_initializer = {init, destroy};
 struct PackageRegistrationManager {
   PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
 } lsvPackageRegistrationManager;
+
+struct myComp{
+  bool operator() (const Abc_Obj_t* a, const Abc_Obj_t* b){
+    return a->Id < b->Id;
+  }
+};
+
+struct myComp1{
+  bool operator() (const set <Abc_Obj_t*, myComp> a, const set <Abc_Obj_t*, myComp> b){
+    return  (*(a.begin()))->Id < (*(b.begin()))->Id;
+  }
+};
+
+void Lsv_Mark(Abc_Obj_t* pObj, bool flag = false){
+    Abc_Obj_t* pFanin;
+    int i;
+    Abc_ObjForEachFanin(pObj, pFanin, i) {
+      // printf("%u\n", pFanin->fMarkA);
+      if (pFanin->fMarkA || pFanin->fMarkB ||  Abc_ObjIsPi(pFanin)){
+        continue;
+      }
+      else{
+        if (Abc_ObjFanoutVec(pFanin)->nSize > 1 || flag){
+          pFanin->fMarkB = 1;
+        }
+        else {
+          pFanin->fMarkA = 1;
+        }
+        Lsv_Mark(pFanin);
+      }
+    }
+}
+
+void Lsv_Construct(Abc_Obj_t* pObj, set <Abc_Obj_t*, myComp>& small){
+    Abc_Obj_t* pFanin;
+    int i;
+    Abc_ObjForEachFanin(pObj, pFanin, i) 
+    {
+      if (pFanin->fMarkA || pFanin->fMarkB ||  Abc_ObjIsPi(pFanin)){
+        continue;
+      }
+      else{
+          pFanin->fMarkA = 1;
+          small.insert(pFanin);
+          Lsv_Construct(pFanin, small);
+      }
+    }
+  
+}
+void msfc_print(unsigned count, set <Abc_Obj_t*, myComp> small){
+  printf("MSFC %u: ", count);
+  set <Abc_Obj_t*, myComp>::iterator iter = small.begin();
+  bool flag = true;
+  while(iter != small.end()){
+    if(flag){
+      flag = false;
+    }
+    else{
+      printf(",");
+    }
+    printf("n%u", (*iter)->Id);
+    ++iter;
+  }
+  printf("\n");
+}
+int Lsv_MSFC(Abc_Frame_t* pAbc, int argc, char** argv){
+  Abc_Obj_t* pPo;
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int i;
+  Abc_NtkForEachPo( pNtk, pPo, i ){
+    Lsv_Mark(pPo, true);
+  }
+  Abc_Obj_t* pObj;
+  Abc_NtkForEachObj(pNtk, pObj, i){
+    pObj->fMarkA = 0;
+  }
+  set <set <Abc_Obj_t*, myComp>, myComp1> to_print;
+  Abc_NtkForEachObj(pNtk, pObj, i){
+    if(pObj->fMarkB == 1){
+      set <Abc_Obj_t*, myComp> small;
+      small.insert(pObj);
+      Lsv_Construct(pObj, small);
+      to_print.insert(small);
+    }
+  }
+  unsigned count = 0;
+  set <set <Abc_Obj_t*, myComp>, myComp1>::iterator iter = to_print.begin();
+  while(iter != to_print.end()){
+    msfc_print(count, *iter);
+    ++count;
+    ++iter;
+  }
+  Abc_NtkForEachObj(pNtk, pObj, i){
+    pObj->fMarkA = 0;
+    pObj->fMarkB = 0;
+  }
+  return 0;
+usage:
+  Abc_Print(-2, "usage: lsv_print_msfc\n");
+  Abc_Print(-2, "\t        prints msfc in the network\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+}
+
+
+
+
+
+
+
+
 
 void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
   Abc_Obj_t* pObj;
