8490185169eda8f71aa39e89559bd4184245116e
HHHUUUGGGOOO <hugo551565@gmail.com>
2021-10-15 11:26:25 +0800

    test basic framework

---
+++ src/ext-lsv/lsv_print_msfc.cpp
@@ -0,0 +1,61 @@
+#include "base/abc/abc.h"
+#include "base/main/main.h"
+#include "base/main/mainInt.h"
+
+// add new command
+static int LSV_CommandPrintMSFC(Abc_Frame_t* pAbc, int argc, char** argv);
+
+void init(Abc_Frame_t* pAbc)
+{
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_msfc", LSV_CommandPrintMSFC, 0);
+}
+
+void destroy(Abc_Frame_t* pAbc) {}
+
+Abc_FramInitializer_t frame_initializer = { init, destroy };
+
+struct PackageRegistrationManager
+{
+  PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
+} lsvPackageRegistrationManager;
+
+
+// main function
+void Lsv_NtkPrintMSFC(Abc_Ntk_t* pNtk)
+{
+  
+}
+
+
+// Define command function : LSV_CommandPrintMSFC
+int LSV_CommandPrintMSFC(Abc_Frame_t* pAbc, int argc, char** argv)
+{
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF)
+  {
+    switch (c)
+    {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk)
+  {
+    Abc_Print(-1, "Empty network.\n");
+    return -1;
+  }
+  // main function
+  Lsv_NtkPrintMSFC(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: lsv_print_msfc [-h]\n");
+  Abc_Print(-2, "\t        prints the msfc in the network\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+
+}
