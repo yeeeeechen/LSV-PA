fd6e59a3c5b339874a3a821a45b1cc8cc73d6c36
leechungchia <[b06901074@ntu.edu.tw]>
2021-10-21 01:37:09 +0800

    pa1 part3

--- src/ext_r10943098/lsvCmd.cpp
+++ src/ext_r10943098/lsvCmd.cpp
@@ -28,9 +28,11 @@ bool obj_set_comparator::operator() (set<Abc_Obj_t*, obj_comparator>* a, set<Abc
 }
 
 static int Lsv_CommandMSFC(Abc_Frame_t* pAbc, int argc, char** argv);
+static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "MSFC", "lsv_print_msfc", Lsv_CommandMSFC, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -156,6 +158,48 @@ int Lsv_CommandMSFC(Abc_Frame_t* pAbc, int argc, char** argv) {
     return 1;
   }
   Lsv_NtkMSFC(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: lsv_print_nodes [-h]\n");
+  Abc_Print(-2, "\t        prints the nodes in the network\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+}
+void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
+  Abc_Obj_t* pObj;
+  int i;
+  Abc_NtkForEachNode(pNtk, pObj, i) {
+    printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
+    Abc_Obj_t* pFanin;
+    int j;
+    Abc_ObjForEachFanin(pObj, pFanin, j) {
+      printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
+             Abc_ObjName(pFanin));
+    }
+    if (Abc_NtkHasSop(pNtk)) {
+      printf("The SOP of this node:\n%s", (char*)pObj->pData);
+    }
+  }
+}
+
+int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
+    switch (c) {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk) {
+    Abc_Print(-1, "Empty network.\n");
+    return 1;
+  }
+  Lsv_NtkPrintNodes(pNtk);
   return 0;
 
 usage:
