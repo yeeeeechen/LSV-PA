f24bd4cd5941285a57651f7848be7705b13b2743
Je-Wei-Chuang <b07901048@ntu.edu.tw>
2021-10-22 03:34:29 +0800

    pa1_3

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,11 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <unordered_map>
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
 
+using namespace std;
+
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
+static int LSV_CommandPrintMSFC(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_msfc", LSV_CommandPrintMSFC, 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -57,4 +66,145 @@ usage:
   Abc_Print(-2, "\t        prints the nodes in the network\n");
   Abc_Print(-2, "\t-h    : print the command usage\n");
   return 1;
+}
+
+bool compareV(Abc_Obj_t* a, Abc_Obj_t* b)
+{
+  return Abc_ObjId(a) < Abc_ObjId(b);
+}
+
+bool compareVV(vector<Abc_Obj_t*>& a, vector<Abc_Obj_t*>& b)
+{
+  return Abc_ObjId(a[0]) < Abc_ObjId(b[0]);
+}
+
+unordered_map<string, int> const_exist;
+
+void TRAVERSE(Abc_Ntk_t* pNtk, Abc_Obj_t* pNode, vector<Abc_Obj_t*>& cone, bool rt)
+{
+    if (pNode -> fMarkA == 1) {return;}
+    else if ((Abc_ObjFanoutNum(pNode) > 1)&&(!rt))
+    {
+        pNode -> fMarkB = 1;
+        return;
+    }
+    else if (Abc_ObjIsCi(pNode))
+    {
+        pNode -> fMarkA = 1;
+        pNode -> fMarkB = 0;
+        return;
+        
+    }
+    else if (!Abc_ObjIsPo(pNode))
+    {
+        cone.push_back(pNode);
+        pNode->fMarkA = 1;
+    }
+    else {pNode->fMarkA = 1;}
+    pNode -> fMarkB = 0;
+    Abc_Obj_t* pFanin;
+    int i;
+    // recursively traverse each fanin
+    Abc_ObjForEachFanin(pNode, pFanin, i) { TRAVERSE(pNtk, pFanin, cone, false); }
+}
+
+void MSFC(Abc_Ntk_t* pNtk)
+{
+    int i;
+    Abc_Obj_t* P_init;
+    Abc_Obj_t* Po;
+    vector<vector<Abc_Obj_t*> > cones;
+    
+    // initialization
+    Abc_NtkForEachNode(pNtk, P_init, i)
+    {
+        P_init -> fMarkA = 0;
+        P_init -> fMarkB = 0;
+    }
+    
+    // primary output
+    Abc_NtkForEachPo(pNtk, Po, i)
+    {
+        vector<Abc_Obj_t*> cone;
+        TRAVERSE(pNtk, Po, cone, true);
+        sort(cone.begin(), cone.end(), compareV);
+        cones.push_back(cone);
+    }
+    
+    while(true)
+    {
+        // check if empty
+        bool exist;
+        exist = false;
+        Abc_Obj_t* P_check;
+        Abc_NtkForEachNode(pNtk, P_check, i)
+        {
+            if (P_check -> fMarkB == 1)
+            {
+                exist = true;
+                break;
+            }
+        }
+        if (exist == false) {break;}
+        
+        // execute
+        Abc_Obj_t* P_ite;
+        Abc_NtkForEachNode(pNtk, P_ite, i)
+        {
+            if (P_ite -> fMarkB == 1)
+            {
+                vector<Abc_Obj_t*> cone;
+                TRAVERSE(pNtk, P_ite, cone, true);
+                sort(cone.begin(), cone.end(), compareV);
+                cones.push_back(cone);
+            }
+        }
+    }
+    
+    sort(cones.begin(), cones.end(), compareVV);
+    
+    int count_ans = 0;
+    for (int k = 0 ; k < cones.size() ; ++k)
+    {
+      printf("MSFC %d: ", count_ans);
+      for (int l = 0 ; l < cones[k].size() ; ++l)
+      {
+        printf("%s", Abc_ObjName(cones[k][l]));
+        if (l == cones[k].size()-1) { printf("\n"); }
+        else { printf(","); }
+      }
+      ++count_ans;
+    }
+}
+
+int LSV_CommandPrintMSFC(Abc_Frame_t* pAbc, int argc, char** argv)
+{
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF)
+  {
+    switch (c)
+    {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk)
+  {
+    Abc_Print(-1, "Empty network.\n");
+    return -1;
+  }
+  // main function
+  MSFC(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: lsv_print_msfc [-h]\n");
+  Abc_Print(-2, "\t        prints the msfc in the network\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+
 }
