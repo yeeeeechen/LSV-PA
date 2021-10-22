99aa04c69c34bb0cf9445da0df30334dcdd39c76
j09188 <55417513+j09188@users.noreply.github.com>
2021-10-22 11:37:48 +0800

    Update msfc.cpp

--- src/ext-lsv/msfc.cpp
+++ src/ext-lsv/msfc.cpp
@@ -33,52 +33,6 @@ void traversal_fanin (Abc_Obj_t* pObj, std::vector <int> &temp, std::vector <str
   }
 }
 
-/*
-void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
-  Abc_Obj_t* pObj;
-  int i;
-  Abc_NtkForEachNode(pNtk, pObj, i) {
-    printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
-    Abc_Obj_t* pFanin;
-    int j;
-    Abc_ObjForEachFanin(pObj, pFanin, j) {
-      printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
-             Abc_ObjName(pFanin));
-    }
-    if (Abc_NtkHasSop(pNtk)) {
-      printf("The SOP of this node:\n%s", (char*)pObj->pData);
-    }
-  }
-}
-*/
-/*
-int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
-  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
-  int c;
-  Extra_UtilGetoptReset();
-  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
-    switch (c) {
-      case 'h':
-        goto usage;
-      default:
-        goto usage;
-    }
-  }
-  if (!pNtk) {
-    Abc_Print(-1, "Empty network.\n");
-    return 1;
-  }
-  Lsv_NtkPrintNodes(pNtk);
-  return 0;
-
-usage:
-  Abc_Print(-2, "usage: lsv_print_nodes [-h]\n");
-  Abc_Print(-2, "\t        prints the nodes in the network\n");
-  Abc_Print(-2, "\t-h    : print the command usage\n");
-  return 1;
-}
-*/
-
 void Lsv_NtkPrintMsfc(Abc_Ntk_t* pNtk) {
   Abc_Obj_t* pObj;
   std::vector <int> test, store_head;
@@ -122,21 +76,6 @@ void Lsv_NtkPrintMsfc(Abc_Ntk_t* pNtk) {
     Abc_NtkForEachNode(pNtk, pObj, i) {
     printf("Object Id = %d, name = %s, type = %d\n", Abc_ObjId(pObj), Abc_ObjName(pObj), Abc_ObjType(pObj));
     }
-    /*
-    Abc_Obj_t* pLatch;
-    Abc_NtkForEachLatchOutput( pNtk, pLatch, i ){
-      printf("Object Id = %d, name = %s\n", Abc_ObjId(pLatch), Abc_ObjName(pLatch));
-    }
-    Abc_Obj_t* pCo;
-    Abc_NtkForEachCo(pNtk, pCo, i){
-      printf("Object Id = %d, name = %s\n", Abc_ObjId(pCo), Abc_ObjName(pCo));
-    }
-    std::cout<<std::endl;
-    Abc_Obj_t* pCi;
-    Abc_NtkForEachCi(pNtk, pCi, i){
-      printf("Object Id = %d, name = %s\n", Abc_ObjId(pCi), Abc_ObjName(pCi));
-    }
-    */
   }
 
   Abc_Obj_t* pObj_2;
