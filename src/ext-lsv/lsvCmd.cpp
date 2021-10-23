3689f414b9f120237cc824ee93f8ac3ddc80ce58
Je-Wei-Chuang <b07901048@ntu.edu.tw>
2021-10-23 12:36:14 +0800

    latesub

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -80,14 +80,12 @@ bool compareVV(vector<Abc_Obj_t*>& a, vector<Abc_Obj_t*>& b)
 
 unordered_map<string, int> const_exist;
 
+// void TRAVERSE(Abc_Ntk_t* pNtk, Abc_Obj_t* pNode, vector<Abc_Obj_t*>& cone, bool rt, vector<Abc_Obj_t*>& cst)
 void TRAVERSE(Abc_Ntk_t* pNtk, Abc_Obj_t* pNode, vector<Abc_Obj_t*>& cone, bool rt)
 {
     if (pNode -> fMarkA == 1) {return;}
-    else if ((Abc_ObjFanoutNum(pNode) > 1)&&(!rt))
-    {
-        pNode -> fMarkB = 1;
-        return;
-    }
+    
+    // base case
     else if (Abc_ObjIsCi(pNode))
     {
         pNode -> fMarkA = 1;
@@ -95,47 +93,83 @@ void TRAVERSE(Abc_Ntk_t* pNtk, Abc_Obj_t* pNode, vector<Abc_Obj_t*>& cone, bool 
         return;
         
     }
-    else if (!Abc_ObjIsPo(pNode))
+    
+    // constant
+    else if (Abc_ObjType(pNode)==1)
     {
-        cone.push_back(pNode);
-        pNode->fMarkA = 1;
+        if (Abc_ObjFanoutNum(pNode) > 1)
+        {
+            // cst.push_back(pNode);
+            return;
+        }
+        else
+        {
+            cone.push_back(pNode);
+        }
+        return;
     }
-    else {pNode->fMarkA = 1;}
+    
+    // new fanout cone
+    else if ((Abc_ObjFanoutNum(pNode) > 1)&&(!rt))
+    {
+        pNode -> fMarkB = 1;
+        return;
+    }
+    
+    // travel
+    else
+    {
+        pNode->fMarkA = 1;
+        cone.push_back(pNode);
+    }
+
     pNode -> fMarkB = 0;
     Abc_Obj_t* pFanin;
     int i;
     // recursively traverse each fanin
+    // Abc_ObjForEachFanin(pNode, pFanin, i) { TRAVERSE(pNtk, pFanin, cone, false, cst); }
     Abc_ObjForEachFanin(pNode, pFanin, i) { TRAVERSE(pNtk, pFanin, cone, false); }
 }
 
 void MSFC(Abc_Ntk_t* pNtk)
 {
-    int i;
+    int i_1;
+    bool done;
+    done = false;
     Abc_Obj_t* P_init;
     Abc_Obj_t* Po;
     vector<vector<Abc_Obj_t*> > cones;
     
     // initialization
-    Abc_NtkForEachNode(pNtk, P_init, i)
+    Abc_NtkForEachNode(pNtk, P_init, i_1)
     {
         P_init -> fMarkA = 0;
         P_init -> fMarkB = 0;
     }
     
-	int j;
+    int i_2;
     // primary output
-    Abc_NtkForEachPo(pNtk, Po, i)
+    Abc_NtkForEachPo(pNtk, Po, i_2)
     {
-        //printf("PO \n");
+        int j_1;
         Po -> fMarkA = 1;
         Abc_Obj_t* pFanin;
-        Abc_ObjForEachFanin(Po, pFanin, j)
+        Abc_ObjForEachFanin(Po, pFanin, j_1)
         {
             //printf("PI \n");
             vector<Abc_Obj_t*> cone;
+            vector<Abc_Obj_t*> cst;
+            // TRAVERSE(pNtk, pFanin, cone, true, cst);
             TRAVERSE(pNtk, pFanin, cone, true);
             sort(cone.begin(), cone.end(), compareV);
             cones.push_back(cone);
+            /*
+            if ((!cst.empty())&&(!done))
+            {
+                cones.push_back(cst);
+                done = true;
+            }
+            */
         }
     }
 
@@ -145,7 +179,8 @@ void MSFC(Abc_Ntk_t* pNtk)
         bool exist;
         exist = false;
         Abc_Obj_t* P_check;
-        Abc_NtkForEachNode(pNtk, P_check, i)
+        int j_2;
+        Abc_NtkForEachNode(pNtk, P_check, j_2)
         {
             if (P_check -> fMarkB == 1)
             {
@@ -157,14 +192,24 @@ void MSFC(Abc_Ntk_t* pNtk)
         
         // execute
         Abc_Obj_t* P_ite;
-        Abc_NtkForEachNode(pNtk, P_ite, i)
+        int j_3;
+        Abc_NtkForEachNode(pNtk, P_ite, j_3)
         {
             if (P_ite -> fMarkB == 1)
             {
-                vector<Abc_Obj_t*> cone;
-                TRAVERSE(pNtk, P_ite, cone, true);
-                sort(cone.begin(), cone.end(), compareV);
-                cones.push_back(cone);
+                vector<Abc_Obj_t*> CONE;
+                vector<Abc_Obj_t*> CST;
+                // TRAVERSE(pNtk, P_ite, CONE, true, CST);
+                TRAVERSE(pNtk, P_ite, CONE, true);
+                sort(CONE.begin(), CONE.end(), compareV);
+                cones.push_back(CONE);
+                /*
+                if ((!CST.empty())&&(!done))
+                {
+                    cones.push_back(CST);
+                    done = true;
+                }
+                */
             }
         }
     }
@@ -184,17 +229,22 @@ void MSFC(Abc_Ntk_t* pNtk)
       ++count_ans;
     }
 
-    Abc_NtkForEachNode(pNtk, P_init, i)
+    int i_3;
+    Abc_NtkForEachNode(pNtk, P_init, i_3)
     {
         P_init -> fMarkA = 0;
         P_init -> fMarkB = 0;
     }
-    Abc_NtkForEachPo(pNtk, P_init, i)
+    
+    int i_4;
+    Abc_NtkForEachPo(pNtk, P_init, i_4)
     {
         P_init -> fMarkA = 0;
         P_init -> fMarkB = 0;
     }
-    Abc_NtkForEachPi(pNtk, P_init, i)
+    
+    int i_5;
+    Abc_NtkForEachPi(pNtk, P_init, i_5)
     {
         P_init -> fMarkA = 0;
         P_init -> fMarkB = 0;
@@ -232,3 +282,4 @@ usage:
   return 1;
 
 }
+
