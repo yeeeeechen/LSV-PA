af6a9f08af0b1d3f15c13fdc5d027f5b2bb01102
Chaoho Hsieh <55687287+HeHH1329@users.noreply.github.com>
2021-12-24 05:22:53 +0800

    PA2 v1
    Add the comment "lsv_or_bidec"

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,4 +1,6 @@
 #include "base/abc/abc.h"
+#include "aig/aig/aig.h"
+#include "sat/cnf/cnf.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
 #include <iostream>
@@ -8,10 +10,12 @@
 using namespace std;
 
 static int Lsv_CommandPrintMSFC(Abc_Frame_t* pAbc, int argc, char** argv);
+static int Lsv_CommandOrBidec(Abc_Frame_t* pAbc, int argc, char** argv);
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_msfc", Lsv_CommandPrintMSFC, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_or_bidec", Lsv_CommandOrBidec, 0);
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
 }
 
@@ -19,11 +23,299 @@ void destroy(Abc_Frame_t* pAbc) {}
 
 Abc_FrameInitializer_t frame_initializer = {init, destroy};
 
-// ---------- lsv_print_msfc ----------
-
 struct PackageRegistrationManager {
   PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
 } lsvPackageRegistrationManager;
+
+// ---------- lsv_or_bidec ----------
+extern "C" {Aig_Man_t * Abc_NtkToDar( Abc_Ntk_t * pNtk, int fExors, int fRegisters);} // see line 233, abcDar.c 
+
+void Lsv_NtkOrBidec(Abc_Ntk_t* pNtk){
+  Abc_Obj_t* pPo;
+  int po_id;
+  Abc_NtkForEachPo(pNtk, pPo, po_id) { // For each PO, we need to know whether it's bi-decomposable
+    // build the CNF of f(X) and f'(X)
+    Abc_Ntk_t* pPoCone = Abc_NtkCreateCone(pNtk, Abc_ObjFanin0(pPo), Abc_ObjName(pPo), 0); // extract the cone of the PO and its support set. Last argument is fUseAllCis 
+    if( Abc_ObjFaninC0(pPo) ) Abc_ObjXorFaninC( Abc_NtkPo(pPoCone, 0), 0 );  
+    Aig_Man_t* pPoCone_aig = Abc_NtkToDar(pPoCone, 0, 0); // AIG of f(X) obtained
+    Cnf_Dat_t* pPo_cnf = Cnf_Derive(pPoCone_aig, Aig_ManCoNum(pPoCone_aig)); // f(X) in CNF form
+    int nCnfVar = pPo_cnf->nVars; // # of variables in f(X), f'(X) and f''(X)
+    int nCi = Aig_ManCiNum(pPoCone_aig); // # of alphas and betas 
+
+    // cout <<"PO cone"<<Abc_NtkName(pPoCone)<<"has "<<Abc_NtkPiNum(pPoCone)<<"PIs\n";
+
+    sat_solver* pSat = (sat_solver*)Cnf_DataWriteIntoSolver(pPo_cnf, 1, 0); // CNF of f(X)
+    sat_solver_setnvars(pSat, 3*nCnfVar+2*nCi);
+    int Cid, i_out;
+    lit lits[3]; 
+    Aig_Obj_t * pVarOut;
+    /*
+    lits[0] = 2*(pPo_cnf->pVarNums[Aig_ManCo(pPoCone_aig, 0)->Id]);
+    Cid = sat_solver_addclause( pSat, lits, lits+1);
+    Cnf_DataLift(pPo_cnf, nCnfVar);
+    Cnf_DataWriteIntoSolverInt(pSat, pPo_cnf, 1, 0); // CNF of neg f'(X)
+    lits[0] = 2*(pPo_cnf->pVarNums[Aig_ManCo(pPoCone_aig, 0)->Id])+1;
+    Cid = sat_solver_addclause( pSat, lits, lits+1);
+    Cnf_DataLift(pPo_cnf, nCnfVar);
+    Cnf_DataWriteIntoSolverInt(pSat, pPo_cnf, 1, 0); // CNF of neg f''(X)
+    lits[0] = 2*(pPo_cnf->pVarNums[Aig_ManCo(pPoCone_aig, 0)->Id])+1;
+    Cid = sat_solver_addclause( pSat, lits, lits+1);
+    */
+    
+    Aig_ManForEachCo(pPoCone_aig, pVarOut, i_out){
+      lits[0] = 2*(pPo_cnf->pVarNums[pVarOut->Id]);
+      Cid = sat_solver_addclause( pSat, lits, lits+1);
+    }
+    Cnf_DataLift(pPo_cnf, nCnfVar);
+    Cnf_DataWriteIntoSolverInt(pSat, pPo_cnf, 1, 0); // CNF of neg f'(X)
+    Aig_ManForEachCo(pPoCone_aig, pVarOut, i_out){
+      lits[0] = 2*(pPo_cnf->pVarNums[pVarOut->Id])+1;
+      Cid = sat_solver_addclause( pSat, lits, lits+1);
+    }
+    Cnf_DataLift(pPo_cnf, nCnfVar);
+    Cnf_DataWriteIntoSolverInt(pSat, pPo_cnf, 1, 0); // CNF of neg f''(X)
+    Aig_ManForEachCo(pPoCone_aig, pVarOut, i_out){
+      lits[0] = 2*(pPo_cnf->pVarNums[pVarOut->Id])+1;
+      Cid = sat_solver_addclause( pSat, lits, lits+1);
+    }
+    
+    // initialize empty SAT solver ordering of variables: 
+    /*  x_1,     neg x_1,     ..., x_n,     neg x_n;     2*nCnfVar
+        x'_1,    neg x'_1,    ..., x'_n,    neg x'_n;    4*nCnfVar
+        x''_1,   neg x''_1,   ..., x''_n,   neg x''_n;   2*alpha_temp
+        alpha_1, neg alpha_1, ..., alpha_n, neg alpha_n; 8*nCnfVar
+        beta_1,  neg beta_1,  ..., beta_n,  neg beta_n   10*nCnfVar
+    */
+    
+    // cout << "start writing a,b clauses:\n";
+    Aig_Obj_t * pVarIn;
+    int pos = 6*nCnfVar+2;
+    int i_in;
+    //vector<int> alphas, betas;
+    //int alpha_temp, beta_temp;
+    Aig_ManForEachCi(pPoCone_aig, pVarIn, i_in){
+      int inId = pPo_cnf->pVarNums[pVarIn->Id];
+      lits[0] = 2*(inId-2*nCnfVar) + 1;  
+      lits[1] = 2*(inId-nCnfVar);
+      lits[2] = pos;
+      sat_solver_addclause(pSat, lits, lits+3);
+      lits[0] = 2*(inId-nCnfVar) + 1;  
+      lits[1] = 2*(inId-2*nCnfVar);
+      lits[2] = pos; 
+      pos += 2; // beta = alpha + 1
+      sat_solver_addclause(pSat, lits, lits+3);
+      lits[0] = 2*(inId-2*nCnfVar) + 1;  
+      lits[1] = 2*inId;
+      lits[2] = pos;
+      sat_solver_addclause(pSat, lits, lits+3);
+      lits[0] = 2*inId + 1;  
+      lits[1] = 2*(inId-2*nCnfVar);
+      lits[2] = pos;
+      sat_solver_addclause(pSat, lits, lits+3);   
+      pos += 2; 
+    }
+    /*
+    Aig_ManForEachCi(pPoCone_aig, pVarIn, i){
+      int inId = pPo_cnf->pVarNums[pVarIn->Id];
+      alpha_temp = sat_solver_addvar(pSat);
+      beta_temp = sat_solver_addvar(pSat);
+      alphas.push_back(alpha_temp);
+      betas.push_back(beta_temp);
+      lits[0] = 2*inId + 1;  
+      lits[1] = 2*nCnfVar + 2*inId;
+      lits[2] = 2*alpha_temp;
+      lits[3] = 2*nCnfVar + 2*inId + 1;  
+      lits[4] = 2*inId;
+      lits[5] = 2*alpha_temp;
+      lits[6] = 2*inId + 1;  
+      lits[7] = 4*nCnfVar + 2*inId;
+      lits[8] = 2*beta_temp;
+      lits[9] = 4*nCnfVar + 2*inId + 1;  
+      lits[10] = 2*inId;
+      lits[11] = 2*beta_temp;
+
+      sat_solver_addclause(pSat, lits, lits+3);
+      sat_solver_addclause(pSat, lits+3, lits+6);
+      sat_solver_addclause(pSat, lits+6, lits+9);
+      sat_solver_addclause(pSat, lits+9, lits+12);      
+    }
+    */
+    /*
+    cout<<"alpha array = [";
+    for (int i=0; i<=alphas.size(); i++) cout<<alphas[i]<<", ";
+    cout<<"]\n";
+    cout<<"beta array = [";
+    for (int i=0; i<=betas.size(); i++) cout<<betas[i]<<", ";
+    cout<<"]\n";
+    */
+    // cout << "start setting seeds:\n";
+    int seed10, seed01; 
+    lit assumptList[2*nCi];
+    int status;
+    for (int i = 0; i < 2*nCi; i++){ 
+      assumptList[i] = 6*nCnfVar + 2 + 2*i + 1;
+    }
+    for (seed10 = 0; seed10 < nCi-1 ; seed10++){
+      for (seed01 = seed10+1; seed01 < nCi; seed01++){ 
+        assumptList[2*seed10]--; // alpha
+        assumptList[2*seed01+1]--; // beta
+        status = sat_solver_solve(pSat, assumptList, assumptList + 2*nCi, 0, 0, 0, 0);
+        // cout <<"Output"<<Abc_ObjName(pPo)<<"under (seed 10, seed 01) = (" << seed10 << ", " << seed01 << ") supports bi-decomp, status = "<<status<<'\n';
+        assumptList[2*seed10]++; // alpha
+        assumptList[2*seed01+1]++; // beta
+        /*
+        int nfinalConflict, * pfinalConflict;
+        nfinalConflict = sat_solver_final(pSat, &pfinalConflict);
+        */
+        assert(status != l_Undef);
+        if(status == -1) break; //unsat -> found some XA | XB | XC
+      }
+      if(status == -1) break; //unsat -> found some XA | XB | XC
+    }
+
+    // output
+    if((status == -1) && Aig_ManCiNum(pPoCone_aig)){ // find the most balanced partition
+      std::cout<<"PO "<<Abc_ObjName(pPo)<<" support partition: 1\n";
+
+      // from now on, assumption list stores the value of alphas and betas for the final de-composition
+      for(int i = 0; i < 2*nCi; i++){ 
+        assumptList[i] = 1; 
+      }
+
+      int *pClauses, *pLit, *pStop;
+      int nFinalClause = sat_solver_final(pSat, &pClauses); // find the final clause 
+
+      // from now on, assumption list stores the value of alphas and betas for the final de-composition
+      for(int i = 0; i < nFinalClause; i++){ 
+        for ( pLit = &pClauses[i], pStop = &pClauses[i+1]; pLit < pStop; pLit++ ){
+          assumptList[Abc_Lit2Var(*pLit)-(nCnfVar*3)-1]=0;
+        } 
+      } 
+
+      //just try to make XA and XB balance
+      int nXA=0, nXB=0;
+      for(int i = 0; i < nCi; i++){
+        if     (assumptList[(2*i)]==0 && assumptList[(2*i)+1]==1) cout<<"1"; //alpha=0, beta=1 => XB
+        else if(assumptList[(2*i)]==1 && assumptList[(2*i)+1]==0) cout<<"2"; //alpha=1, beta=0 => XA
+        else if(assumptList[(2*i)]==0 && assumptList[(2*i)+1]==0) cout<<"0"; //alpha=0, beta=0 => XC
+        else if(nXA>nXB){ cout<<"1"; ++nXB; } 
+        else            { cout<<"2"; ++nXA; }  
+      }
+      cout<<"\n";
+    }
+    else{
+      cout<<"PO "<<Abc_ObjName(pPo)<<" support partition: 0\n";
+    }
+    /*
+    for (seed10 = 0; seed10 < nCi-1 ; seed10++){
+      for (seed01 = seed10+1; seed01 < nCi; seed01++){ 
+        // assumption list 
+        for (int i=0; i < nCi; i++){
+          if (i == seed10){
+            assumptList[i*2] = 6*nCnfVar;
+            assumptList[i*2+1] = 6*nCnfVar + 2*nCi + 1;
+          }
+          else if (i == seed01){
+            assumptList[i*2] = 6*nCnfVar + 1;
+            assumptList[i*2+1] = 6*nCnfVar + 2*nCi;
+          }
+          else {
+            assumptList[i*2] = 6*nCnfVar + 1;
+            assumptList[i*2+1] = 6*nCnfVar + 2*nCi + 1;
+          }
+        }
+        int status = sat_solver_solve(pSat, assumptList, assumptList + 2*nCi, 0, 0, 0, 0);
+        cout <<"Output"<<Abc_ObjName(pPo)<<"under (seed 10, seed 01) = (" << seed10 << ", " << seed01 << ") supports bi-decomp: "<<status<<'\n';
+        int nfinalConflict, * pfinalConflict;
+        nfinalConflict = sat_solver_final(pSat, &pfinalConflict);
+      }
+    }
+    */
+    // Create alpha, beta clauses in the following form:
+    // (neg x_i | x_i' | alpha_i)(neg x_i' | x_i | alpha_i)(neg x_i | x_i'' | beta_i)(neg x_i'' | x_i | beta_i)
+    /*
+    for (int clause_id = 0; clause_id < nVarTotal; clause_id++){
+      int lits[4][3]; // lits[0], lits[1]: alpha; lits[2], lits[3]: beta
+      lits[0][0] = 2*clause_id + 1;  
+      lits[0][1] = 2*nVarTotal + 2*clause_id;
+      lits[0][2] = 6*nVarTotal + 2*clause_id;
+      lits[1][0] = 2*nVarTotal + 2*clause_id + 1;  
+      lits[1][1] = 2*clause_id;
+      lits[1][2] = 6*nVarTotal + 2*clause_id;
+      lits[2][0] = 2*clause_id + 1;  
+      lits[2][1] = 4*nVarTotal + 2*clause_id;
+      lits[2][2] = 8*nVarTotal + 2*clause_id;
+      lits[3][0] = 4*nVarTotal + 2*clause_id + 1;  
+      lits[3][1] = 2*clause_id;
+      lits[3][2] = 8*nVarTotal + 2*clause_id; 
+      sat_solver_addclause(pSat, &lits[0][0], &lits[0][2]);
+      sat_solver_addclause(pSat, &lits[1][0], &lits[1][2]);
+      sat_solver_addclause(pSat, &lits[2][0], &lits[2][2]);
+      sat_solver_addclause(pSat, &lits[3][0], &lits[3][2]);
+    }
+    int seed10, seed01; 
+    lit assumptList[2*nVarTotal];
+    int status;
+    for (seed10 = 0; seed10 < nVarTotal-1 ; seed10++){
+      for (seed01 = seed10+1; seed01 < nVarTotal; seed01++){ 
+        // assumption list 
+        for (int i=0; i < nVarTotal; i++){
+          if (i == seed10){
+            assumptList[i*2] = 6*nVarTotal + 2*i;
+            assumptList[i*2+1] = 8*nVarTotal + 2*i + 1;
+          }
+          else if (i == seed01){
+            assumptList[i*2] = 6*nVarTotal + 2*i + 1;
+            assumptList[i*2+1] = 8*nVarTotal + 2*i;
+          }
+          else {
+            assumptList[i*2] = 6*nVarTotal + 2*i + 1;
+            assumptList[i*2+1] = 8*nVarTotal + 2*i + 1;
+          }
+        }
+        int status = sat_solver_solve(pSat, assumptList, assumptList + 2*nVarTotal, 0, 0, 0, 0);
+        cout <<"Output"<<Abc_ObjName(pPo)<<"under (seed 10, seed 01) = (" << seed10 << ", " << seed01 << ") supports bi-decomp: "<<status<<'\n';
+        int nfinalConflict; 
+        int * pfinalConflict;
+        nfinalConflict = sat_solver_final(pSat, &pfinalConflict);
+      }
+    } 
+    */
+    // Implement the seed variable partition
+    
+
+  }
+
+  // Use 
+}
+
+int Lsv_CommandOrBidec(Abc_Frame_t* pAbc, int argc, char** argv) {
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
+    switch (c) {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk) {
+    Abc_Print(-1, "Empty network.\n");
+    return 1;
+  }
+  Lsv_NtkOrBidec(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: lsv_or_bidec [-h]\n");
+  Abc_Print(-2, "\t        determine and print the bi-decomposition of the output cone.\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+}
+
+// ---------- lsv_print_msfc ----------
 
 bool compareByID (Abc_Obj_t* Obj1, Abc_Obj_t* Obj2){
   return (Abc_ObjId(Obj1) < Abc_ObjId(Obj2) );
