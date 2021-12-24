dbfec698ed7c35610d07f70592224ac4d08c1f77
ChilsonJ <joe00529@gmail.com>
2021-12-24 12:25:48 +0800

    update ext-lsv/lsvCmd.cpp for pa2

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,13 +1,15 @@
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
+#include "sat/cnf/cnf.h"
+#include "sat/bsat/satSolver.h"
 
-static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
-static int Lsv_CommandPrintMSFC(Abc_Frame_t* pAbc, int argc, char** argv);
+extern "C" Aig_Man_t * Abc_NtkToDar( Abc_Ntk_t * pNtk, int fExors, int fRegisters );
+
+static int Lsv_CommandORBiDec(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
-  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
-  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_msfc", Lsv_CommandPrintMSFC, 0);
+    Cmd_CommandAdd(pAbc, "LSV", "lsv_or_bidec", Lsv_CommandORBiDec, 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -15,169 +17,171 @@ void destroy(Abc_Frame_t* pAbc) {}
 Abc_FrameInitializer_t frame_initializer = {init, destroy};
 
 struct PackageRegistrationManager {
-  PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
-} lsvPackageRegistrationManager;
+    PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
+} lsvPackageRegistrationManager2;
 
-void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
-  Abc_Obj_t* pObj;
-  int i;
-  Abc_NtkForEachNode(pNtk, pObj, i) {
-    printf("Object Id = %d, name = %s\n, #FO = %d", Abc_ObjId(pObj), Abc_ObjName(pObj), Abc_ObjFanoutNum(pObj));
-    Abc_Obj_t* pFanin;
-    int j;
-    Abc_ObjForEachFanin(pObj, pFanin, j) {
-      printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
-             Abc_ObjName(pFanin));
+void Lsv_ORBiDec(Abc_Ntk_t* pNtk) {
+    Abc_Obj_t* pObj;
+    Abc_Ntk_t* PONtk;
+    Aig_Man_t* pAig;
+    Cnf_Dat_t* pCnf;
+    sat_solver* pSat;
+    int i, j, k;
+    int *begin,*end;
+    int aigPoId, originCnfVar, originCnfId;
+    pNtk = Abc_NtkStrash(pNtk, 1, 1, 0);
+    Abc_NtkForEachPo(pNtk, pObj, i) {    
+        Aig_Obj_t* supportObj;
+        lit Lit[3];
+        PONtk = Abc_NtkCreateCone(pNtk, Abc_ObjFanin0(pObj), Abc_ObjName(pObj), 0);
+        if(Abc_ObjFaninC0(pObj)) {
+            Abc_ObjXorFaninC(Abc_NtkPo(PONtk, 0), 0);
+        }
+        PONtk = Abc_NtkStrash(PONtk, 1, 1, 0);
+        pAig = Abc_NtkToDar(PONtk, 0, 0);
+        pCnf = Cnf_Derive(pAig, 1);
+        pSat = (sat_solver*)Cnf_DataWriteIntoSolver(pCnf, 1, 0);
+
+        aigPoId = Aig_ManCo(pAig, 0)->Id;
+        originCnfId = pCnf->pVarNums[aigPoId];
+        originCnfVar = pCnf->nVars;
+
+        Lit[0] = toLitCond(originCnfId, 0);
+        sat_solver_addclause(pSat, Lit, Lit+1);
+
+        Cnf_DataLift(pCnf, originCnfVar);
+        Cnf_CnfForClause(pCnf, begin, end, j) {
+            sat_solver_addclause(pSat, begin, end);
+        }
+        Lit[0] = toLitCond(originCnfId+originCnfVar, 1);
+        sat_solver_addclause(pSat, Lit, Lit+1);
+        Cnf_DataLift(pCnf, originCnfVar);
+        Cnf_CnfForClause(pCnf, begin, end, j) {
+            sat_solver_addclause(pSat, begin, end);
+        }
+        Lit[0] = toLitCond(originCnfId + originCnfVar*2, 1);
+        sat_solver_addclause(pSat, Lit, Lit+1);
+        int alphaBegin = sat_solver_nvars(pSat);
+        int alphaSize = Aig_ManCiNum(pAig);
+        Aig_ManForEachCi(pAig, supportObj, k) {
+            int alpha = sat_solver_addvar(pSat);
+            int beta = sat_solver_addvar(pSat);
+            int x = pCnf->pVarNums[Aig_ObjId(supportObj)] - 2*originCnfVar;
+            int xx = x + originCnfVar;
+            int xxx = x + originCnfVar*2;
+            Lit[0] = toLitCond(x, 1);
+            Lit[1] = toLitCond(xx, 0);
+            Lit[2] = toLitCond(alpha, 0);
+            sat_solver_addclause(pSat, Lit, Lit+3);
+            Lit[0] = toLitCond(x, 0);
+            Lit[1] = toLitCond(xx, 1);
+            Lit[2] = toLitCond(alpha, 0);
+            sat_solver_addclause(pSat, Lit, Lit+3);
+            Lit[0] = toLitCond(x, 1);
+            Lit[1] = toLitCond(xxx, 0);
+            Lit[2] = toLitCond(beta, 0);
+            sat_solver_addclause(pSat, Lit, Lit+3);
+            Lit[0] = toLitCond(x, 0);
+            Lit[1] = toLitCond(xxx, 1);
+            Lit[2] = toLitCond(beta, 0);
+            sat_solver_addclause(pSat, Lit, Lit+3);
+        }
+        lbool status = l_True;
+        int alpha,beta;
+        lit* assumps = new lit[2*alphaSize];
+        for(int a = 0; a < alphaSize; ++a) {
+            for(int b = a+1; b < alphaSize; ++b) {
+                for(int q = 0; q < alphaSize; ++q) {
+                    alpha = alphaBegin + 2*q;
+                    beta = alpha + 1;
+                    if(q == a) {
+                        assumps[q] = toLitCond(alpha, 0);
+                        assumps[alphaSize+q] = toLitCond(beta, 1);
+                    }
+                    else if(q == b) {
+                        assumps[q] = toLitCond(alpha, 1);
+                        assumps[alphaSize+q] = toLitCond(beta, 0);
+                    }
+                    else {
+                        assumps[q] = toLitCond(alpha, 1);
+                        assumps[alphaSize+q] = toLitCond(beta, 1);
+                    }
+                }
+                status = sat_solver_solve(pSat, assumps, assumps + 2*alphaSize,0,0,0,0);
+                if(status == l_False) {
+                    break;
+                }
+            }
+            if(status == l_False) {
+                break;
+            }
+        }
+        int *pFinalConf;
+        int solSize = 0;
+        int solution[alphaSize][2];
+        for(int i = 0; i < alphaSize; ++i) {
+            solution[i][0] = 0;
+            solution[i][1] = 0;
+        }
+        // 0 -> Xc, 1 -> Xb, 2 -> Xa
+        if(status == l_False) {
+            solSize = sat_solver_final(pSat, &pFinalConf);
+            for(int solIdx = 0; solIdx < solSize; ++solIdx) {
+                int varible = (pFinalConf[solIdx]/2);
+                solution[(varible-alphaBegin)/2][varible%2] = 1;
+            }
+            printf("PO %s support partition: 1\n",Abc_ObjName(pObj));
+            for(int i = 0; i < alphaSize; ++i) {
+                int sol_alpha = solution[i][0];
+                int  sol_beta = solution[i][1];
+                if(sol_alpha == 1 && sol_beta == 1) {
+                    // 0,0 -> Xc 
+                    printf("0");
+                }
+                else if(sol_alpha == 1 && sol_beta == 0) {
+                    // 0,1 -> Xb
+                    printf("2");
+                }
+                else if(sol_alpha == 0 && sol_beta == 1) {
+                    // 0,1 -> Xa
+                    printf("1");
+                }
+                else {
+                    // 1,1 -> Xa | Xb
+                    printf("0");
+                }
+            }
+            printf("\n");
+        }
+        else {
+            printf("PO %s support partition: 0\n",Abc_ObjName(pObj));
+        }
+        delete []assumps;
     }
-    if (Abc_NtkHasSop(pNtk)) {
-      printf("The SOP of this node:\n%s", (char*)pObj->pData);
-    }
-  }
 }
 
-int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
-  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
-  int c;
-  Extra_UtilGetoptReset();
-  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
-    switch (c) {
-      case 'h':
-        goto usage;
-      default:
-        goto usage;
+int Lsv_CommandORBiDec(Abc_Frame_t* pAbc, int argc, char** argv) {
+    Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+    int c;
+    Extra_UtilGetoptReset();
+    while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
+        switch (c) {
+            case 'h':
+                goto usage;
+            default:
+                goto usage;
+        }
     }
-  }
-  if (!pNtk) {
-    Abc_Print(-1, "Empty network.\n");
-    return 1;
-  }
-  Lsv_NtkPrintNodes(pNtk);
-  return 0;
+    if (!pNtk) {
+        Abc_Print(-1, "Empty network.\n");
+        return 1;
+    }
+    Lsv_ORBiDec(pNtk);
+    return 0;
 
 usage:
-  Abc_Print(-2, "usage: lsv_print_nodes [-h]\n");
-  Abc_Print(-2, "\t        prints the nodes in the network\n");
-  Abc_Print(-2, "\t-h    : print the command usage\n");
-  return 1;
-}
-
-// compare using the obj Id, assuming vector of Abc_Obj_t*
-static int Lsv_VecPtrSortCompare1( void ** pp1, void ** pp2 ) {
-  if ( Abc_ObjId((Abc_Obj_t*)(*pp1)) < Abc_ObjId((Abc_Obj_t*)(*pp2)))
-    return -1;
-  if ( Abc_ObjId((Abc_Obj_t*)(*pp1)) > Abc_ObjId((Abc_Obj_t*)(*pp2))) 
+    Abc_Print(-2, "usage: lsv_bi_decomposition [-h]\n");
+    Abc_Print(-2, "\t        non-trivial or bi-decomposition\n");
+    Abc_Print(-2, "\t-h    : print the command usage\n");
     return 1;
-  return 0; 
 }
-
-// compare using the first obj Id, assuming vector of (vector of Abc_Obj_t*)
-static int Lsv_VecPtrSortCompare2(void ** pp1, void ** pp2) {
-  if( Abc_ObjId((Abc_Obj_t*)Vec_PtrEntry((Vec_Ptr_t*)(*pp1), 0)) <
-      Abc_ObjId((Abc_Obj_t*)Vec_PtrEntry((Vec_Ptr_t*)(*pp2), 0))) {
-    return -1;
-  }
-  if( Abc_ObjId((Abc_Obj_t*)Vec_PtrEntry((Vec_Ptr_t*)(*pp1), 0)) >
-      Abc_ObjId((Abc_Obj_t*)Vec_PtrEntry((Vec_Ptr_t*)(*pp2), 0))) {
-    return 1;
-  }
-  return 0; 
-}
-
-
-void Lsv_AigMSFC_rec(Abc_Obj_t * pNode, Vec_Ptr_t * vNodes, Vec_Ptr_t* msfc, bool isRoot) {
-    Abc_Obj_t * pFanin;
-    int i;
-
-    if ( Abc_NodeIsTravIdCurrent(pNode) || pNode->fMarkA==1) { return; }
-    // mark the node as visited
-    Abc_NodeSetTravIdCurrent(pNode);
-    // skip the PI
-    if (Abc_ObjIsCi(pNode)) { return ;}
-    // push node with multiple fanout to the back of vNodes
-    if (Abc_ObjFanoutNum(pNode) > 1 && !isRoot) {
-      //printf("%s has %d fanout\n", Abc_ObjName(pNode), Abc_ObjFanoutNum(pNode));
-      Vec_PtrPush(vNodes, pNode);
-      return;
-    }
-
-    // visit the transitive fanin of the node
-    Abc_ObjForEachFanin(pNode, pFanin, i)
-      Lsv_AigMSFC_rec(pFanin, vNodes, msfc, 0);
-    // visit the equivalent nodes
-    // if ( Abc_AigNodeIsChoice( pNode ) )
-    //     for ( pFanin = (Abc_Obj_t *)pNode->pData; pFanin; pFanin = (Abc_Obj_t *)pFanin->pData )
-    //         Abc_AigDfs_rec( pFanin, vNodes );
-    // add the node after the fanins have been added
-    if(Abc_ObjIsPo(pNode)) { return; }
-    Vec_PtrPush(msfc, pNode);
-    pNode->fMarkA = 1;
-}
-
-Vec_Ptr_t * Lsv_AigMSFC(Abc_Ntk_t * pNtk) {
-  Vec_Ptr_t * msfcVec;
-  Vec_Ptr_t * vNodes;
-  Abc_Obj_t * pNode;
-  int i;
-  assert(Abc_NtkIsStrash(pNtk));
-  Abc_NtkCleanMarkA(pNtk);
-
-  vNodes = Vec_PtrAlloc(100);
-  msfcVec = Vec_PtrAlloc(100);
-  // go through the PO nodes and call for each of them
-  Abc_NtkForEachCo(pNtk, pNode, i) {Vec_PtrPush(vNodes, pNode);}
-  Vec_PtrForEachEntry(Abc_Obj_t*, vNodes, pNode, i) {
-    Abc_NtkIncrementTravId(pNtk);
-    Vec_Ptr_t * msfc = Vec_PtrAlloc(50);
-    Lsv_AigMSFC_rec(pNode, vNodes, msfc, 1);
-    Vec_PtrSort(msfc, (int (*)(const void *, const void *)) Lsv_VecPtrSortCompare1);
-    if(Vec_PtrSize(msfc)>0)
-      Vec_PtrPush(msfcVec, (void*)msfc);
-  }
-  Abc_NtkCleanMarkA(pNtk); 
-  return msfcVec;
-}
-
-void Lsv_NtkPrintMSFC(Abc_Ntk_t* pNtk) {
-  Vec_Ptr_t* msfcVec;
-  Vec_Ptr_t* pVec;
-  Abc_Obj_t* pNode;
-  int i, j;
-  msfcVec = Lsv_AigMSFC(pNtk);
-  Vec_PtrSort(msfcVec, (int (*)(const void *, const void *)) Lsv_VecPtrSortCompare2);
-  Vec_PtrForEachEntry(Vec_Ptr_t*, msfcVec, pVec, i) {
-    printf("MSFC %d: ", i);
-    Vec_PtrForEachEntry(Abc_Obj_t*, pVec, pNode, j) {
-      printf("%s", Abc_ObjName(pNode));
-      if(j != Vec_PtrSize(pVec) - 1) printf(",");
-    }
-    printf("\n");
-  }
-}
-
-int Lsv_CommandPrintMSFC(Abc_Frame_t* pAbc, int argc, char** argv) {
-  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
-  assert(Abc_NtkIsStrash(pNtk));
-  int c;
-  Extra_UtilGetoptReset();
-  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
-    switch (c) {
-      case 'h':
-        goto usage;
-      default:
-        goto usage;
-    }
-  }
-  if (!pNtk) {
-    Abc_Print(-1, "Empty network.\n");
-    return 1;
-  }
-  Lsv_NtkPrintMSFC(pNtk);
-  return 0;
-
-usage:
-  Abc_Print(-2, "usage: lsv_print_msfc [-h]\n");
-  Abc_Print(-2, "\t        prints the sorted MSFC group in the aig\n");
-  Abc_Print(-2, "\t-h    : print the command usage\n");
-  return 1;
-}
-
